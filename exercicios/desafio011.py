6e0f8cbeb1bf559a26b4295d54cc5cad79372600
Lucas Galindo Martins <165849134+LucasGalindoMartins@users.noreply.github.com>
2025-04-03 15:10:43 -0300

    desafio11

---
+++ exercicios/desafio011.py
@@ -0,0 +1,5 @@
+height = float(input('Qual a altura da parede? '))
+width = float(input('Qual a largura da parede? '))
+area = height * width
+li = area / 2
+print(f'Você irá precisar de {li:.2f} litros de tinta para pintar essa parede!')
