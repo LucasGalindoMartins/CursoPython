0ab3750971bcde9e2ebe470e5ba93c45b4ed7d76
Lucas Galindo Martins <165849134+LucasGalindoMartins@users.noreply.github.com>
2025-04-04 15:06:01 -0300

    Update desafio06.py

--- exercicios/desafio06.py
+++ exercicios/desafio06.py
@@ -2,4 +2,4 @@ n = int(input('Digite um número: '))
 du = n * 2
 tri = n * 3
 r = n ** (1/2)
-print(f'Dobro: {du} \nTriplo: {tri} \nRaiz Quadrada: {r}')
+print(f'Dobro: {du} \nTriplo: {tri} \nRaiz Quadrada: {r:.2f}')
