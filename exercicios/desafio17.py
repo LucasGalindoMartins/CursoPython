3cd0db28e92d7eedb955d96511563ac49f94044a
Lucas Galindo Martins <165849134+LucasGalindoMartins@users.noreply.github.com>
2025-04-07 20:23:50 -0300

    Ajustando desafio 17

--- exercicios/desafio17.py
+++ exercicios/desafio17.py
@@ -1,5 +1,10 @@
-from math import sqrt
+from math import hypot
 co = float(input('Digite o cateto oposto do triângulo: '))
 ca = float(input('Digite o cateto adjacente do triângulo: '))
-hip = (co**2) + (ca**2)
-print(f'A hipotenusa desse triângulo é {sqrt(hip)}')
+hip = hypot(co, ca)
+print(f'A hipotenusa desse triângulo é {hip:.2f}')
+
+# co = float(input('Comprimento do cateto oposto: '))
+# ca = float(input('Comprimento do cateto adjacente: '))
+# hi = (co ** 2 + ca ** 2) ** (1/2)
+# print(f'A hipotenusa vai medir {hi:.2f}')
