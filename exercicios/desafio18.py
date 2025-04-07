f310cec8ee43274d2fb8f856b6fe05985da1d0cb
Lucas Galindo Martins <165849134+LucasGalindoMartins@users.noreply.github.com>
2025-04-07 20:31:09 -0300

    Ajustando desafio 18

--- exercicios/desafio18.py
+++ exercicios/desafio18.py
@@ -7,4 +7,4 @@ cos = math.cos(rad)
 sen = math.sin(rad)
 tan = math.tan(rad)
 
-print(f'Cosseno: {round(cos, 6)}\nSeno: {round(sen, 6)}\nTangente: {round(tan, 6)}')
+print(f'Cosseno: {cos:.2f}\nSeno: {sen:.2f}\nTangente: {tan:.2f}')
