16a5f704296b0fab2c55eec944af5e5bd35ad5cb
Lucas Galindo Martins <165849134+LucasGalindoMartins@users.noreply.github.com>
2025-04-07 20:16:20 -0300

    Ajustando desafio 16

--- exercicios/desafio16.py
+++ exercicios/desafio16.py
@@ -1,3 +1,6 @@
-import math
+from math import trunc
 n = float(input('Digite um número real: '))
-print(math.floor(n))
+print(f'A porção inteira do valor digitado é {trunc(n)}')
+
+# n = float(input('Digite um valor: '))
+# print(f'O valor digitado foi {n} e sua porção inteira é {int(n)}')
