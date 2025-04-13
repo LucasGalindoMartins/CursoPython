2ba9d4518b1c300802cdcf781ccef34baedfdeeb
Lucas Galindo Martins <165849134+LucasGalindoMartins@users.noreply.github.com>
2025-04-13 16:06:44 -0300

    aula09 - fatiando strings

---
+++ aula09.py
@@ -0,0 +1,32 @@
+frase = 'Curso em Vídeo Python'
+# frase = frase.replace('Python', 'Android')
+# dividido = frase.split()
+
+#Primeiro numero (começo), segundo (final), terceiro(pulando de quanto em quanto)
+
+# print(frase[3:15]) 
+
+# print(frase[15:])
+
+# print(frase[:15])
+
+# print(frase[::2])
+
+# print("""ewncwcnwevnwvwnuvwnvuwnvwnvnw
+# wnvinwvnwiovnwivnwivnwiovnwivnwv
+# wnviuwenviowevniwnvwenviownvionwvi
+# wvnowenvwnvwnviwvnivnwivnwiovnw""")
+
+# print(frase.upper().count('O'))
+
+# print(len(frase.strip()))
+
+# print(frase.replace('Python', 'Android'))
+
+# print('Curso' in frase)
+
+# print(frase.find('Curso'))
+
+# print(dividido[0])
+
+# print(dividido[0] [3])
