08c3897a8e54934560a07a0604c72c2aa4f920b6
Lucas Galindo Martins <165849134+LucasGalindoMartins@users.noreply.github.com>
2025-04-18 15:29:12 -0300

    ajustes desafio 28

--- exercicios/desafio28.py
+++ exercicios/desafio28.py
@@ -1,12 +1,25 @@
-from random import choice
+# from random import choice
 
-lista = [0, 1, 2, 3, 4, 5]
-n = choice(lista)
+# lista = [0, 1, 2, 3, 4, 5]
+# n = choice(lista)
 
-escolha = int(input('Tente adivinhar o número de 0 até 5: '))
+# escolha = int(input('Tente adivinhar o número de 0 até 5: '))
 
-if escolha == n:
-    print('Você acertou o número. GANHOU!!')
+# if escolha == n:
+#     print('Você acertou o número. GANHOU!!')
+# else:
+#     print(f'Você errou, o número certo era {n}. PERDEU!!')
+
+from random import randint
+from time import sleep
+comp = randint(0, 5)
+print('-=-' * 10)
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar')
+print('-=-' * 10)
+player = int(input('Em que número eu pensei? '))
+print('PROCESSANDO...')
+sleep(2)
+if player == comp:
+     print('Você acertou o número. GANHOU!!')
 else:
-    print(f'Você errou, o número certo era {n}. PERDEU!!')
-
+    print(f'Você errou, o número certo era {comp}. PERDEU!!')
