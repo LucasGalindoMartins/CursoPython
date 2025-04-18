2282768cd9820a8e71da00adb1ea42974580e34d
Lucas Galindo Martins <165849134+LucasGalindoMartins@users.noreply.github.com>
2025-04-18 15:40:40 -0300

    ajustes desafio 32

--- exercicios/desafio32.py
+++ exercicios/desafio32.py
@@ -1,6 +1,11 @@
-ano = int(input('Digite um ano: '))
+from datetime import date
+
+ano = int(input('Digite um ano: Coloque 0 para analisar o ano atual: '))
+
+if ano == 0:
+    ano = date.today().year
 
 if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
-    print('Ano Bissexto!!')
+    print(f'{ano} Ano Bissexto!!')
 else:
-    print('Ano NÃO bissexto!!')
+    print(f'{ano} Ano NÃO bissexto!!')
