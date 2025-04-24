610bc7ab66783b04872dbca26beaa75f5288eac8
Lucas Galindo Martins <165849134+LucasGalindoMartins@users.noreply.github.com>
2025-04-24 14:54:40 -0300

    desafio 38

---
+++ exercicios/desafio38.py
@@ -0,0 +1,9 @@
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo número: '))
+
+if n1 > n2:
+    print('O PRIMEIRO número é MAIOR')
+elif n2 > n1:
+    print('O SEGUNDO número é MAIOR')
+else:
+    print('Os números são IGUAIS')
