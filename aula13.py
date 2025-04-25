1d145b20bd61160bcbc484d4663bf68a2aa373b3
Lucas Galindo Martins <165849134+LucasGalindoMartins@users.noreply.github.com>
2025-04-25 11:58:39 -0300

    aula 13

---
+++ aula13.py
@@ -0,0 +1,27 @@
+# Contagem normal
+# for c in range(0, 8):
+#     print(c)
+# print('FIM')
+
+# Contagem regressiva
+# for c in range(10 , 0, -1):
+#     print(c)
+# print('FIM')
+
+#Contagem em mais
+# for c in range(0, 7, 2):
+#     print(c)
+# print('FIM')
+
+# i = int(input('Início: '))
+# f = int(input('Fim: '))
+# p = int(input('Passo: '))
+# for c in range(i, f+1, p):
+#     print(c)
+# print('FIM')
+
+s = 0
+for c in range(0 , 4):
+    n = int(input('Digite um valor: '))
+    s += n
+print(f'O somatório de todos os valores foi {s}')
