0c077e778942892ca936d7ce727c64a877444ebb
Lucas Galindo Martins <165849134+LucasGalindoMartins@users.noreply.github.com>
2025-05-08 16:35:09 -0300

    desafio 72

---
+++ exercicios/desafio72.py
@@ -0,0 +1,24 @@
+num = int(input('Digite um número entre 0 e 20: '))
+cont = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete',
+        'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze',
+        'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+
+while True:
+    if num < 0 or num > 20:
+        num = int(input('Tente novamente. Digite um número entre 0 e 20: '))
+    else:
+        print(f'Você digitou o número {cont[num]}.')
+        break
+
+# PROFESSOR
+# cont = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete',
+#         'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze',
+#         'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+
+# while True:
+#     num = int(input('Digite um número entre 0 e 20: '))
+#     if 0 <= num <= 20:
+#         break
+#     print('Tente novamente. ', end='')
+# print(f'Você digitou o número {cont[num]}.')
+
