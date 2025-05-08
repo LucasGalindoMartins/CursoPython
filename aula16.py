d6f8fa434e83858647aeaf2584a229522b7b41df
Lucas Galindo Martins <165849134+LucasGalindoMartins@users.noreply.github.com>
2025-05-08 11:38:28 -0300

    Aula 16

---
+++ aula16.py
@@ -0,0 +1,29 @@
+# lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim', 'Batata Frita')
+# Tuplas são imutáveis
+
+# print(lanche[:2])
+# print(lanche[2:])
+# print(lanche[-1])
+
+# for c in range(0, len(lanche)):
+#     print(f'Eu vou comer {lanche[c]} na posição {c}')
+
+# for c in lanche:
+#     print(f'Eu vou comer {c})
+
+# for pos, c in enumerate(lanche):
+#      print(f'Eu vou comer {c} na posição {pos}')
+
+# print(sorted(lanche))
+
+# a = (2, 5 ,4)
+# b = (5, 8, 1, 2)
+# c = b + a
+# print(len(c)) # Tamanho
+# print(c.count(5)) # contar quantidades
+# print(c)
+# print(c.index(5)) # achar a posição
+# print(c.index(5, 1)) # achar a posição a partir de certo ponto
+
+# pessoa = ('Lucas', 19, 'M', 70)
+# print(pessoa)
