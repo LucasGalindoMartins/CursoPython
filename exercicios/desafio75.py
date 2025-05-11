7321896e29d7a5586e26858a7b92ae50bca1ab37
Lucas Galindo Martins <165849134+LucasGalindoMartins@users.noreply.github.com>
2025-05-11 19:11:24 -0300

    desafio 75

--- exercicios/desafio75.py
+++ exercicios/desafio75.py
@@ -1,16 +1,16 @@
-n1 = int(input('Digite um número: '))
-n2 = int(input('Digite outro número: '))
-n3 = int(input('Digite mais um número: '))
-n4 = int(input('Digite o último número: '))
+num = (int(input('Digite um número: ')),
+       int(input('Digite outro número: ')),
+       int(input('Digite mais um número: ')),
+       int(input('Digite o último número: ')))
 
-numeros = (n1, n2, n3, n4)
-
-print(f'Você digitou os valores {numeros}')
-print(f'O valor 9 apareceu {numeros.count(9)} vezes')
-print(f'O valor 3 apareceu na {numeros.index(3)+1} posição')
-
-# cont = 0
-# while cont in range(0, len(numeros)):
-#     if numeros[cont] % 2 == 0:
-#         cont += 1
-# print(f'Os valores pares digitados foram {cont}')
+print(f'Você digitou os valores {num}.')
+print(f'O valor 9 apareceu {num.count(9)} vezes.')
+if 3 in num:
+    print(f'O valor 3 apareceu na {num.index(3)+1} posição.')
+else:
+    print('O valor 3 não foi digitado.')
+c = 0
+for n in num:
+    if n % 2 == 0:         
+        c += 1
+print(f'Quantidade de valores pares digitados: {c}')
