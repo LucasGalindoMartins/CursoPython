936d84c2ccfd1c761c9d7b9a504239016d911c55
Lucas Galindo Martins <165849134+LucasGalindoMartins@users.noreply.github.com>
2025-05-31 15:55:07 -0300

    aula 20

---
+++ aula20.py
@@ -0,0 +1,59 @@
+def linha():
+     print('-'*30)
+
+# # def titulo(txt):
+# #     print('-'*30)
+# #     print(txt)
+# #     print('-'*30)
+
+
+# # # Programa Principal
+# # titulo('       CURSO EM VÍDEO      ')
+# # titulo('       APRENDA PYTHON      ')
+# # titulo('       GUSTAVO GUANABARA   ')
+
+# def soma(a, b):
+#     print(f'A = {a} e B = {b}')
+#     s = a + b
+#     print(f'A soma de A + B = {s}')
+
+# linha()
+# soma(a=4, b=5)
+# linha()
+# soma(8, 9)
+# linha()
+# soma(2, 1)
+# linha()
+
+# def cont(*num):
+#     for valor in num:
+#         print(f'{valor} -> ', end='')
+#     print('FIM')
+#     print(f'Quantidade de números: {len(num)}')
+    
+# linha()
+# cont(5, 7, 3, 1, 4)
+# linha()
+# cont(8, 4, 7)
+# linha()
+
+# def dobra(lst):
+#      pos = 0
+#      while pos < len(lst):
+#           lst[pos] *= 2
+#           pos += 1
+
+
+# valores = [6, 3, 9, 1, 0, 2]
+# dobra(valores)
+# print(valores)
+
+# def  soma(* valores):
+#     s = 0
+#     for num in valores:
+#          s += num
+#     print(f'Somando os valores {valores} temos {s}')
+
+
+# soma(5, 2)
+# soma(2, 9, 4)
