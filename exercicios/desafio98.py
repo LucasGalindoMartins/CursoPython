b93de8df447ec08969da352995c0485c7867ba79
Lucas Galindo Martins <165849134+LucasGalindoMartins@users.noreply.github.com>
2025-06-05 15:51:13 -0300

    desafio 98

---
+++ exercicios/desafio98.py
@@ -0,0 +1,33 @@
+from time import sleep
+
+def contador(i, f, p):
+    if p < 0:
+        p *= -1
+    if p == 0:
+        p = 1
+    if i < f:
+        print(f'Contagam de {i} até {f} de {p} em {p}')
+        for c in range(i, f+1, p):
+            print(c, end=' ', flush=True)
+            # sleep(0.5)
+        print('FIM!!')
+    else:
+        print(f'Contagam de {i} até {f} de {p} em {p}')
+        for c in range(i, f-1, -p):
+            print(c, end=' ', flush=True)
+            # sleep(0.5)
+        print('FIM!!')
+        
+
+
+print('-='*30)
+contador(1, 10, 1)
+print('-='*30)
+contador(10, 0, -2)
+print('-='*30)
+print('Agora é sua vez de personalizar a contagem!')
+i = int(input('Início: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+print('-='*30)
+contador(i, f, p)
